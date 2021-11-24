a6ab9eb08518b1ba036b270926e2c23246c47d5b
ClevelandSchemer <samjsmith1996@gmail.com>
2021-11-24 05:59:12 -0500

    Made a hash of things.

--- code/hash.h
+++ code/hash.h
@@ -12,6 +12,7 @@ typedef s32 bool32;
 typedef double Value;
 
 #define internal static
+#define global_variable static
 
 struct string
 {
@@ -23,7 +24,8 @@ struct string
 
 struct HashTableEntry
 {
-    string *key;
+    char *key;
+    // TODO: cache len in the entry?
     Value value;
 };
 
@@ -39,7 +41,7 @@ struct HashTable
 
 void init_table(HashTable *table);
 u32 hash(char *key, size_t length);
-bool32 table_set(HashTable *table, string *key, Value value);
+bool32 table_set(HashTable *table, char *key, Value value);
 string *find_string(HashTable *table, char *key, size_t length, u32 hash);
 
 #endif
